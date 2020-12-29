8780e83c281ffb3b856efb751f98686b5861e0ca
rcsmit <rcx.smit@gmail.com>
2020-12-29 22:23:03 +0100

    Update newagebullshitgenerator.py

--- newagebullshitgenerator.py
+++ newagebullshitgenerator.py
@@ -247,7 +247,7 @@ if st.sidebar.button('GENERATE'):
 
 tekst = (
     '<hr>Made by Rene Smit. (<a href=\'http://www.twitter.com/rcsmit\'>@rcsmit</a>) <br>'
-    'Scripts are used from various meditation videos on Youtube<br>'
+  
     'Sourcecode : <a href=\"https://github.com/rcsmit/newagebullshitgenerator/edit/main/newagebullshitgenerator.py\">github.com/rcsmit</a><br><br>' 
     '© 2014-15 Seb Pearce (sebpearce.com)<br>'
     'Licensed under the MIT License.')
