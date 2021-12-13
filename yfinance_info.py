6749839ce316389d4b7495ab2bc3ab1133d02dff
rcsmit <rcx.smit@gmail.com>
2021-12-13 23:08:53 +0700

    x

--- yfinance_info.py
+++ yfinance_info.py
@@ -19,6 +19,9 @@ class QuantGaloreData:
 
         self.data = yf.download(tickers=(choice),period=period,interval=interval,group_by='ticker',auto_adjust=True,prepost=False)
         self.df = pd.DataFrame(self.data)
+        if len(self.df) == 0:
+            st.error("No data or wrong input")
+            st.stop()
         self.df['rownumber'] = np.arange(len(self.df))
         self.choice = choice
 
@@ -40,13 +43,23 @@ class QuantGaloreData:
             y = df["Close"].to_list()
             m,b,r_sq = find_slope_scipy(x,y)
             df['trendline'] = (df['rownumber'] *m +b)
-            z = st.sidebar.number_input("Z-value", 0.0,3.0,1.96)
-            df['trendline_low'] = (df['rownumber'] *m +b) - z * std
-            df['trendline_high'] = (df['rownumber'] *m +b) + z * std
+            z1 = st.sidebar.number_input("Z-value 1", 0.0,3.0,1.0)
+            z2 = st.sidebar.number_input("Z-value 2", 0.0,3.0,1.96)
+            if z1 >= z2:
+                st.warning("Z1 has to be smaller than Z2")
+                st.stop()
+            df['trendline_low_1'] = (df['rownumber'] *m +b) - z1 * std
+            df['trendline_high_1'] = (df['rownumber'] *m +b) + z1 * std
+            df['trendline_low_2'] = (df['rownumber'] *m +b) - z2 * std
+            df['trendline_high_2'] = (df['rownumber'] *m +b) + z2 * std
             df['z_from_mean'] = (df['Close'] - mean) / std
-            return df
+            df["z_from_trendline"] =  (df['Close'] - df['trendline']) / std
 
-        def plot_figure1(df, choice):
+
+
+            return df, std, mean, m, b,z1, z2
+
+        def plot_figure1(df, choice,m,b, std):
 
             low = go.Scatter(
                 name='Low',
@@ -77,15 +90,24 @@ class QuantGaloreData:
                 fillcolor='rgba(68, 68, 68, 0.1)',
                 )
 
-
-            trendline_low = go.Scatter(
+            trendline_low_2 = go.Scatter(
                 name='trendline low',
                 x=df["Date"],
-                y=df['trendline_low'] ,
+                y=df['trendline_low_2'] ,
                 mode='lines',
                 line=dict(width=0.5,
                         color="rgba(255, 255, 0, 0.8)"),
-                fillcolor='rgba(255,255,0 0.05)',
+                fillcolor='rgba(255,255,0,0.2)',
+                fill='tonexty')
+
+            trendline_low_1 = go.Scatter(
+                name='trendline low',
+                x=df["Date"],
+                y=df['trendline_low_1'] ,
+                mode='lines',
+                line=dict(width=0.5,
+                        color="rgba(255, 255, 0, 0.0)"),
+                fillcolor='rgba(255,255,0, 0.4)',
                 fill='tonexty')
 
             trendline = go.Scatter(
@@ -93,80 +115,138 @@ class QuantGaloreData:
                 x=df["Date"],
                 y=df["trendline"],
                 mode='lines',
-                line=dict(width=0.9,color='rgba(255,255,0,1)'),
-                fillcolor='rgba(255,255,0 0.05)',
+                line=dict(width=0.9,color='rgba(255,165,0,1)'),
+                fillcolor='rgba(255,255,0,0.4)',
                 fill='tonexty'
                 )
 
-            trendline_high = go.Scatter(
-                name='trendline high',
+            trendline_high_1 = go.Scatter(
+                name='trendline high 1',
                 x=df["Date"],
-                y=df['trendline_high'] ,
+                y=df['trendline_high_1'] ,
+                mode='lines',
+                line=dict(width=0.5,
+                        color="rgba(255, 255, 0, 0.0)"),
+                fillcolor='rgba(255,255,0, 0.2)',
+                  fill='tonexty'
+                )
+            trendline_high_2 = go.Scatter(
+                name='trendline high 2',
+                x=df["Date"],
+                y=df['trendline_high_2'] ,
                 mode='lines',
                 line=dict(width=0.5,
                         color="rgba(255, 255, 0, 0.8)"),
-                fillcolor='rgba(255,255,255 0.05)',
+                fillcolor='rgba(255,255,0, 0.0)',
+                   fill='tonexty'
+
                 )
 
 
 
-            data = [high, close, low,trendline_high, trendline, trendline_low ]
+            data = [high, close, low,trendline_high_2,trendline_high_1, trendline, trendline_low_1,trendline_low_2 ]
 
             layout = go.Layout(
                 yaxis=dict(title="USD"),
-                title=f"{choice}",)
+                title=f"{choice} | trendline = {round(m,1)} * x + {round(b,1)} | std = {round(std,2)}",)
                 #, xaxis=dict(tickformat="%d-%m")
             fig1 = go.Figure(data=data, layout=layout)
+
+
             fig1.update_layout(xaxis=dict(tickformat="%d-%m-%Y"))
+
             #fig.show()
             st.plotly_chart(fig1, use_container_width=True)
 
 
-        def plot_figure_z_scores(df,choice):
+
+        def plot_figure_z_scores(df,choice,  std, mean, from_what):
             z_score = go.Scatter(
                 name="Close",
                 x=df["Date"],
-                y=df["z_from_mean"],
+                y=df[from_what],
                 mode='lines',
                 line=dict(width=0.75,color='rgba(0,0,255, 0.8)'),
-                fillcolor='rgba(68, 68, 68, 0.2)',
-                fill='tonexty')
+                #fillcolor='rgba(68, 68, 68, 0.2)',
+                #fill='tonexty'
+                )
 
             data = [z_score ]
-
+            if from_what =="z_from_mean":
+                title = f"Z from mean | mean = {round(mean,2)} | std = {round(std,2)}"
+            else:
+                title = f"Z from Trendline  | std = {round(std,2)}"
             layout = go.Layout(
                 yaxis=dict(title=f"Z-score {choice}"),
-                title="Z from mean",)
+                title=title)
             fig1 = go.Figure(data=data, layout=layout)
             fig1.update_layout(xaxis=dict(tickformat="%d-%m-%Y"))
             st.plotly_chart(fig1, use_container_width=True)
 
+        def buy_or_sell(df, std, z1,z2):
+
+            # z_from_trendline = (df['trendline'].tail(1) - df['Close'].tail(1)) / std
+            z_from_trendline =  round(df.z_from_trendline.iat[-1],2) #  (  df.Close.iat[-1] - df.trendline.iat[-1] ) / std
+            st.write (f"Z-score value from trendline = {z_from_trendline}")
+
+            if z_from_trendline <= -1*z2:
+                st.write ("REALLY BUY !")
+            elif (z_from_trendline > -1*z2) and (z_from_trendline <= -1*z1):
+                st.write (" BUY !")
+            elif (z_from_trendline > -1*z1) and (z_from_trendline <= 1*z1):
+                st.write ("HOLD !")
+            elif (z_from_trendline > 1*z1) and (z_from_trendline <= 2*z2):
+                st.write ("Maybe SEL !")
+            elif z_from_trendline > z2:
+                st.write ("REALLY BUY !")
+            st.write("This advice is just a joke ofcourse. Just use your own knowledge and insights.")
+
+
+
+
+        def find_z_mean(self):
+            # This will call the yahoo finance API and store Bitcoin’s OHLC data on the time interval we set.
+            # You can enter in any ticker from all asset classes (futures, stocks, crypto), but for this example,
+            #  we’ll use Bitcoin. Next, we will create a function to calculate the z-score values.
+
+            mean = self.df['Close'].mean()
+            z_from_mean = (self.df['Close'].tail(1) - mean) / np.std(self.df['Close'])
+            print("BTC-USD",self.df['Close'].tail(1),z_from_mean)
+            # This will calculate the mean value and then it will pull the last minute’s trading price
+            # and divide that by the standard deviation of the dataset. Finally, this will return a calculated z-score.
+
 
         st.header("Y Finance charts")
-        df = manipulate_df(self.df)
+        df, std, mean,m,b, z1,z2 = manipulate_df(self.df)
         choice = self.choice
-        plot_figure1(df, choice)
-        plot_figure_z_scores(df, choice)
+        plot_figure1(df, choice,m,b, std)
+        buy_or_sell(df, std, z1,z2)
+
+        plot_figure_z_scores(df, choice,  std, mean, "z_from_trendline")
+        plot_figure_z_scores(df, choice,  std, mean, "z_from_mean")
 
         print (df)
 
 
-    def find_z(self):
-        # This will call the yahoo finance API and store Bitcoin’s OHLC data on the time interval we set.
-        # You can enter in any ticker from all asset classes (futures, stocks, crypto), but for this example,
-        #  we’ll use Bitcoin. Next, we will create a function to calculate the z-score values.
 
-        mean = self.df['Close'].mean()
-        z_from_mean = (self.df['Close'].tail(1) - mean) / np.std(self.df['Close'])
-        print("BTC-USD",self.df['Close'].tail(1),z_from_mean)
-        # This will calculate the mean value and then it will pull the last minute’s trading price
-        # and divide that by the standard deviation of the dataset. Finally, this will return a calculated z-score.
+
 def main():
 
     QGD = QuantGaloreData()
     #QGD.print_dataframe()
     QGD.plt_dataframe()
 
-    st.write("Inspired by : https://medium.com/the-financial-journal/the-million-dollar-algorithm-straight-from-wall-street-3f88a62e3e0a")
-    st.write ("Read disclaimer at https://pypi.org/project/yfinance/")
+    st.write()
+
+    tekst = (
+        "<style> .infobox {  background-color: lightblue; padding: 5px;}</style>"
+        "<hr><div class='infobox'>Made by Rene Smit. (<a href='http://www.twitter.com/rcsmit' target=\"_blank\">@rcsmit</a>) <br>"
+        "Inspired by : <a href='https://medium.com/the-financial-journal/the-million-dollar-algorithm-straight-from-wall-street-3f88a62e3e0a'>I Needed Money, So I Wrote An Algorithm</a> <br>"
+        "Sourcecode : <a href='https://github.com/rcsmit/COVIDcases/blob/main/covid_dashboard_rcsmit.py' target='_blank'>github.com/rcsmit</a><br>"
+        "How-to tutorial : <a href='https://rcsmit.medium.com/making-interactive-webbased-graphs-with-python-and-streamlit-a9fecf58dd4d/' target='_blank'>rcsmit.medium.com</a><br>"
+         "Read <a href=<'https://pypi.org/project/yfinance/'>disclaimer</a> at of Yfinace"
+       )
+
+
+    st.sidebar.markdown(tekst, unsafe_allow_html=True)
 main()
