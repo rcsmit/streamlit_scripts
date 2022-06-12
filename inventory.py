df6b2c7a736879d0826595da95574d666bd4dc36
Rene Smit <rcx.smit@gmail.com>
2022-06-12 17:27:26 +0200

    .

--- inventory.py
+++ inventory.py
@@ -36,7 +36,6 @@ def show_df(df):
             .blank {display:none;width:0px}
             </style>
             """
-
     # Inject CSS with Markdown
     st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
     #st.write(df)
@@ -47,30 +46,7 @@ def show_link():
     url_ = f"<a href='{url}' target='_blank'>Link to Google Sheet</a>"
     st.sidebar.markdown(url_, unsafe_allow_html=True)
 
-def main():
-    df = read()
-    accotype_possible = ["Waikiki","Fiji","Sahara","Kalahari 1","Kalahri 2","Serengeti XL","Serengetti L"]
-    accotype_chosen =  st.sidebar.multiselect("Acotype",accotype_possible, "Waikiki")
-    
-    accotype_str = " & ".join([str(item) for item in accotype_chosen])
-    
-    languages_possible = ["Nederlands", "English","Deutsch","Italiano","Franҁais", "Dansk", "Polski"]
-    languages_chosen = st.sidebar.multiselect("Languages", languages_possible ,["Nederlands", "English"])
-    st.header(f"Inventory for {accotype_str} at Camping De Schatberg")
-    
-    for a in accotype_chosen:
-        df[a] = df[a].fillna(0)
-        if (len(accotype_chosen) ==1):
-            df = df[df[a]>0] #TO DO : filter out when multiple acco's have 0 pcs of a certain item
-        df[a] = df[a].astype(int)
-    to_show =  languages_chosen + accotype_chosen
-    file_name = "_".join([str(item) for item in to_show])
-    df = df[to_show]
-    df = df.reset_index(drop=True)
-    show_df(df)
-    download_button(df, file_name)
-    show_link()
-
+def show_disclaimer(languages_possible, languages_chosen):
     disclaimer_nl = "Lijst is indicatief en niet juridisch bindend"
     disclaimer_en = "List is indicative and not legally binding"
     disclaimer_fr = "La liste est indicative et non juridiquement contraignante"
@@ -85,5 +61,34 @@ def main():
             index = languages_possible.index(l)
             st.write(f" * {disclaimers[index]}")
 
+
+def main():
+    accotype_possible = ["Waikiki","Fiji","Sahara","Kalahari 1","Kalahri 2","Serengeti XL","Serengetti L"]
+    languages_possible = ["Nederlands", "English","Deutsch","Italiano","Franҁais", "Dansk", "Polski"]
+    
+    accotype_chosen =  st.sidebar.multiselect("Accotype",accotype_possible, "Waikiki")
+    languages_chosen = st.sidebar.multiselect("Languages", languages_possible ,["Nederlands", "English"])
+    
+    accotype_str = " & ".join([str(item) for item in accotype_chosen])
+    st.header(f"Inventory for {accotype_str} at Camping De Schatberg")
+    
+    df = read()
+    
+    for a in accotype_chosen:
+        df[a] = df[a].fillna(0)
+        if (len(accotype_chosen) ==1): #TO DO : filter out when multiple acco's have 0 pcs of a certain item
+            df = df[df[a]>0] 
+        df[a] = df[a].astype(int)
+    to_show =  languages_chosen + accotype_chosen
+    file_name = "_".join([str(item) for item in to_show])
+    df = df[to_show]
+    df = df.reset_index(drop=True)
+    show_df(df)
+    show_disclaimer(languages_possible, languages_chosen)
+
+    # sidebar
+    download_button(df, file_name)
+    show_link()
+    
 if __name__ == "__main__":
     main()
