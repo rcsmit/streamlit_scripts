afa15f874897ac82e8562003f8c92c96b5483e0d
Rene Smit <rcx.smit@gmail.com>
2022-09-09 01:03:50 +0200

    .

--- make_breezer_taal.py
+++ make_breezer_taal.py
@@ -1,7 +1,7 @@
 # klein scriptje om een string in hoofdletters om te zetten
 import streamlit as st
 
-def make_breezer_taal(string):
+def make_breezer_taal(string, replace_to_numbers):
     """Make breezerlanguage. 
 
     Args:
@@ -13,14 +13,15 @@ def make_breezer_taal(string):
     new_string = ""
     teller = 0
     for s in string:   
-        if s == "e":
-            new_string += "3"
-        elif s == "i":
-            new_string += "!"
-        elif s == "o":
-            new_string += "0"
-        elif s == "a":
-            new_string +="4"
+        if replace_to_numbers:
+            if s == "e":
+                new_string += "3"
+            elif s == "i":
+                new_string += "!"
+            elif s == "o":
+                new_string += "0"
+            elif s == "a":
+                new_string +="4"
         else:
             if teller %2 ==0:
                 new_string += s.upper()
@@ -33,8 +34,10 @@ def main():
     st.header("Breezertaal converter")
     standard_string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nunc orci, euismod nec venenatis sit amet, pulvinar sed nibh. Donec condimentum id nunc ultrices maximus."
     string = st.sidebar.text_input("String to convert (in lowercase)" , standard_string)
-
-    new_string = make_breezer_taal(string)
+    replace_to_numbers = st.sidebar.selectbox(
+        "Replace to numbers", [True, False], index=0
+        )
+    new_string = make_breezer_taal(string, replace_to_numbers)
     st.write (new_string)
 
 if __name__ == "__main__":
