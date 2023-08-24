a7d544dbaa6a029b44007e0bd515f79bf613190a
Rene Smit <rcx.smit@gmail.com>
2023-08-24 23:40:48 +0200

    .

--- meat_consumption.py
+++ meat_consumption.py
@@ -1,3 +1,5 @@
+# REPRODUCING https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8881926/
+
 import pandas as pd
 import streamlit as st
 import numpy as np
@@ -6,17 +8,29 @@ import plotly.express as px
 
 from scipy.stats import linregress
 
+# from sklearn import linear_model
 
-# REPRODUCING https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8881926/
+import statsmodels.api as sm
+
 
 def get_data(join_how):
+    """Gets the data and merges the dataframes
+
+    Args:
+        join_how (str): "inner" or "outer"
+
+    Returns:
+        df: the complete dataframe
+    """    
     url_meat = "https://raw.githubusercontent.com/rcsmit/streamlit_scripts/main/input/meat_consumption.csv"
     url_gm = "https://raw.githubusercontent.com/rcsmit/streamlit_scripts/main/input/gapminder_data_graphs.csv"
     url_health =  "https://raw.githubusercontent.com/rcsmit/streamlit_scripts/main/input/health_efficiency_index.csv"
- 
+    url_education = "https://raw.githubusercontent.com/rcsmit/streamlit_scripts/main/input/mean-years-of-schooling-long-run.csv"
+    
     # url_meat = r"C:\Users\rcxsm\Documents\python_scripts\streamlit_scripts\input\meat_consumption.csv"
     # url_gm = r"C:\Users\rcxsm\Documents\python_scripts\streamlit_scripts\input\gapminder_data_graphs.csv"
     # url_health = r"C:\Users\rcxsm\Documents\python_scripts\streamlit_scripts\input\health_efficiency_index.csv"
+    # url_education = r"C:\Users\rcxsm\Downloads\mean-years-of-schooling-long-run.csv"
     
     
     df =  pd.read_csv(url_meat, delimiter=',')
@@ -43,10 +57,13 @@ def get_data(join_how):
     # https://web.archive.org/web/20200313135813/https://www.who.int/healthinfo/paper30.pdf
 
     df_health = pd.read_csv(url_health, delimiter=',')
-    st.write(df_health)
     df_health = df_health[["health_eff_index_rank" ,"country","health_eff_index"]]
 
-    final_merged_df = merged_df.merge(df_health, on="country", how=join_how)
+    final_merged_df_ = merged_df.merge(df_health, on="country", how=join_how)
+   
+    df_education =  pd.read_csv(url_education, delimiter=',')
+    df_education = df_education[df_education["year"] == 2020]
+    final_merged_df = final_merged_df_.merge(df_education, on="country", how=join_how)
    
 
     # # Find countries in df but not in df_gm
@@ -60,11 +77,23 @@ def get_data(join_how):
     # st.write(countries_in_df_not_in_df_gm)
 
     # st.write("Countries in df_health but not in df:")
+    st.subheader("DATA")
     st.write(final_merged_df)
     st.write(f"Lengte {len(final_merged_df)}")
     return final_merged_df
 
 def make_scatterplot(df_, x, y, show_log_x,show_log_y,trendline_per_continent):
+    """Makes a scatterplot
+
+    Args:
+        df_ (df): the dataframe
+        x (str): column used for x axis
+        y (str): column used for y axis
+        show_log_x (bool): _description_
+        show_log_y (bool): _description_
+        trendline_per_continent (bool): Do we want a trendline per continent
+    """    
+    st.subheader("Scatterplot")
     df = df_.dropna(subset=[x,y])
     # Create a DataFrame (df) using your data
     # Calculate linear regression
@@ -95,29 +124,100 @@ def make_scatterplot(df_, x, y, show_log_x,show_log_y,trendline_per_continent):
     st.plotly_chart(fig)
     
 def correlation_matrix(df):
+    """Generates and shows a correlation matrix and a heatmap
 
+    Args:
+        df (_type_): _description_
+    """
     columns_meat =   ["meat_cons","life_exp_birth","life_exp_5","mort_under_5","cal_day","gdpppp _2011","urban_pop","bmi_over_30","cho_crops","prim_educ_over_25"]
     columns_health = ["health_eff_index_rank" ,"health_eff_index"]
     columns_gm = ["life_exp","hdi_index","co2_consump","gdp","services"]
-    columns = columns_meat + columns_health + columns_gm
+    columns_educ = ["avg_years_ed"]
+    columns = columns_meat + columns_health + columns_gm +columns_educ
 
     df = df[columns]
     corrMatrix = df.corr()
-    #sn.heatmap(corrMatrix, annot=True, annot_kws={"fontsize": 7})
+    st.subheader("Correlation matrix")
     st.write(corrMatrix)
+    fig = px.imshow(corrMatrix.abs()) 
+    st.plotly_chart(fig)
+
+
+def multiple_lineair_regression(df_):
+    """Calculates multiple lineair regression. User can choose the Y value and the X values
+
+    Args:
+        df_ (df): df with info
+    """    
+    st.subheader("Multiple Lineair Regression")
+    y_value = st.selectbox("Y value", ['life_exp',"life_exp_birth","life_exp_5","mort_under_5"],0)
+    x_values_options =  ["meat_cons","cal_day","gdpppp _2011","urban_pop","bmi_over_30","cho_crops","prim_educ_over_25","health_eff_index_rank" ,"health_eff_index","hdi_index","co2_consump","gdp","services", 'avg_years_ed']
+    x_values_default = ['meat_cons','health_eff_index','avg_years_ed',"urban_pop","bmi_over_30","cho_crops","prim_educ_over_25"]
+    x_values = st.multiselect("X values", x_values_options, x_values_default)
+    
+    df = df_.dropna(subset=x_values)
+    df = df.dropna(subset=y_value)
+    df =df[["country"]+[y_value]+ x_values]
+    st.write("**DATA**")
+    st.write(df)
+    st.write(f"Length : {len(df)}")
+    x = df[x_values]
+    y = df[y_value]
+    
+    # with statsmodels
+    x = sm.add_constant(x) # adding a constant
+    
+    model = sm.OLS(y, x).fit()
+    #predictions = model.predict(x) 
+    st.write("**OUTPUT**")
+    print_model = model.summary()
+    st.write(print_model)
+    
 def main():
-    join_how = "inner"
+    """Main function
+    """    
+    join_how = "outer"
     df = get_data(join_how)
     df["continent"].fillna("UNKNOWN", inplace=True)
-    df, x,y, show_log_x, show_log_y, trendline_per_continent = interface()
-    
+    df, x,y, show_log_x, show_log_y, trendline_per_continent = interface(df)
     
     make_scatterplot(df, x, y, show_log_x,show_log_y,trendline_per_continent)
     correlation_matrix(df)
-    st.info('''Meat consumption etc. : https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8881926/ (appendix 1)\n            Gapminder data set, values from 2018 : https://www.kaggle.com/datasets/albertovidalrod/gapminder-dataset?resource=download\n
-            Health Efficiency Index : https://web.archive.org/web/20200313135813/https://www.who.int/healthinfo/paper30.pdf''')
+    multiple_lineair_regression(df)
+    show_footer()
+
+def show_footer():
+    """Shows the footer
+    """    
+    st.subheader("Data Sources")
+    st.info("Meat consumption etc. : https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8881926/ (appendix 1)")
+    st.write("newborn life expectancy (e(0)), life expectancy at 5 years of life (e(5)) and intakes of meat, and carbohydrate crops, respectively. The established risk factors to life expectancy – caloric intake, urbanization, obesity and education levels – were included as the potential confounders.")
+    
+    st.info("Gapminder data set, values from 2018 : https://www.kaggle.com/datasets/albertovidalrod/gapminder-dataset?resource=download")
+    st.write("Country (country). Describes the country name")
+    st.write("Continent (continent). Describes the continent to which the country belongs")
+    st.write("Year (year). Describes the year to which the data belongs")
+    st.write("Life expectancy (life_exp). Describes the life expectancy for a given country in a given year")
+    st.write("Human Development Index (hdi_index). Describes the HDI index value for a given country in a given year")
+    st.write("--- The Human Development Index (HDI) is a statistical composite index of life expectancy, education (mean years of schooling completed and expected years of schooling upon entering the education system), and per capita income indicators, which is used to rank countries into four tiers of human development. ")
+    st.write("CO2 emissions per person(co2_consump). Describes the CO2 emissions in tonnes per person for a given country in a given year")
+    st.write("Gross Domestic Product per capita (gdp). Describes the GDP per capita in dollars for a given country in a given year")
+    st.write("% Service workers (services). Describes the the % of service workers for a given country in a given year")
+    st.info("Health Efficiency Index : https://web.archive.org/web/20200313135813/https://www.who.int/healthinfo/paper30.pdf")
+    st.write("Factors : 25% for health (DALE), 25% for health inequality, 12.5% for the level of responsiveness, 12.5% for the distribution of responsiveness, and 25% for fairness in financing.")
+    st.info("Education: https://ourworldindata.org/human-development-index")
+    st.write("Entity,Code,Year,Combined - average years of education for 15-64 years male and female youth and adults")
 
 def interface(df):
+    """Makes the interface
+
+    Args:
+        df (df): dataframe
+
+    Returns:
+        
+        df,x,y,show_log_x,show_log_y,trendline_per_continent : the various values
+    """    
     x = st.sidebar.selectbox('Select a column X', df.columns[1:], 0)
     y = st.sidebar.selectbox('Select a column Y', df.columns[1:], 1)
     continents = df["continent"].unique()
