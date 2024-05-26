35a8a959debbaf49d52b3cc76b0841cf7ec9cd35
Rene Smit <rcx.smit@gmail.com>
2024-05-26 21:35:01 +0200

    .

--- yepcheck.py
+++ yepcheck.py
@@ -229,6 +229,7 @@ def fetch_dochters_dropdown(db_path):
 
 def main():
     db_path = "input\\yepcheck.sqlite"
+    st.write("x")
     display_database_contents(db_path)
     #display_table_and_column_names(db_path)
     #delete_rows_except_first_100(db_path, "mailinglist")
