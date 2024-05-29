340dd993c21d26692cf4ed778f42f79d1100b6f0
Rene Smit <rcx.smit@gmail.com>
2024-05-29 17:51:36 +0200

    .

--- show_knmi_functions/neerslagtekort.py
+++ show_knmi_functions/neerslagtekort.py
@@ -117,9 +117,9 @@ def neerslagtekort(df):
     df = neerslagtekort_(df)
     
     plot_neerslagtekort(df)
-    spaghetti_plot(df, ['neerslag_etmaalsom'], 7, 7, False, False, True, False, True, False, "Greys", False)
-    spaghetti_plot(df, ['neerslag_etmaalsom'], 7, 7, False, False, True, False, True, False, "Greys", True)
-    spaghetti_plot(df, ['temp_max'], 7, 7, False, False, True, False, True, False, "Greys", False)
+    spaghetti_plot(df, ['neerslag_etmaalsom'], 3, 3, False, False, True, False, True, False, "Greys", False)
+    spaghetti_plot(df, ['neerslag_etmaalsom'], 3, 3, False, False, True, False, True, False, "Greys", True)
+    spaghetti_plot(df, ['temp_max'], 3, 3, False, False, True, False, True, False, "Greys", False)
 
 def get_dataframe(FROM, UNTIL):
     
