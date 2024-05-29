a7f1289ca01264922de48dc67bf5a2c8b1c60166
Rene Smit <rcx.smit@gmail.com>
2024-05-29 20:58:17 +0200

    .

--- show_knmi_functions/utils.py
+++ show_knmi_functions/utils.py
@@ -153,6 +153,7 @@ def get_data(url):
         #divide_by_10 = False if platform.processor() else True
         divide_by_10 = True
         if divide_by_10:
+            st.write(url)
             for d in to_divide_by_10:
                 df['neerslag_etmaalsom'].replace(" ", 0, inplace=True)
                 df[d] = pd.to_numeric(df[d])
