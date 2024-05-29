9c2c41ed7403aac6960f2a44e6e0d09020447d28
Rene Smit <rcx.smit@gmail.com>
2024-05-29 20:41:15 +0200

    .

--- show_knmi_functions/neerslagtekort.py
+++ show_knmi_functions/neerslagtekort.py
@@ -115,6 +115,7 @@ def neerslagtekort_(df):
     df['month'] = df['YYYYMMDD'].dt.month
     #df = df[(df['month'] >= 4) & (df['month'] <= 9)]
     st.write(df)
+    
     # Applying the function
     df["eref"] = df.apply(lambda row: makkink(row["temp_avg_sma"],row["temp_max_sma"], row["glob_straling"]), axis=1)
     df["neerslagtekort"] =     df["eref"] -df["neerslag_etmaalsom"]
