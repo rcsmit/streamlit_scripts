439cc7300cb3c9f0ea5ff5892495ed155659998d
Rene Smit <rcx.smit@gmail.com>
2025-02-14 13:10:01 +0700

    ..

--- human_design_chart.py
+++ human_design_chart.py
@@ -3,11 +3,12 @@ import swisseph as swe
 
 import pandas as pd
 import datetime
-
-
+from skyfield.api import load
+import ephem 
 """
 
 https://github.com/jdempcy/hdkit converted to Python
+uses also https://onecompiler.com/python/3z7ecjyb5
 
 """
 
@@ -113,7 +114,7 @@ def main_kerykeion(y,m,d,h,min, birthplace, birthcountry, delta):
         mean_node = item.mean_node
         true_node = item.true_node
         chiron =item.chiron
-        pl = [sun,moon, mercury, venus,mars,jupiter,saturn,uranus, neptune, pluto, mean_node, true_node, chiron]
+        pl = [sun,moon, mean_node, true_node, mercury, venus,mars,jupiter,saturn,uranus, neptune, pluto,chiron ]
                 
         # Create a list of dictionaries
         
@@ -140,10 +141,43 @@ def main_kerykeion(y,m,d,h,min, birthplace, birthcountry, delta):
             }
             data_list.append(data)
 
+
+        # planet_positions[planet] = {"sign": planet_sign, "degree": planet_degree}
+        #     # Calculate South Node and Earth positions based on North Node and Sun positions
+        # sn_sign, sn_degree = get_sign_and_degree((planet_positions['North Node']['degree'] + 180) % 360)
+        # planet_positions['South Node'] = {"sign": sn_sign, "degree": sn_degree}
+        # data = {
+        #         'item' :item_t,
+        #         'Name': name,
+        #         'Sign': sign,
+        #         'Pos_dec' : pos_dec,
+        #         'Degree': degree,
+        #         'Minutes': minutes,
+        #         'Seconds': seconds
+        #     }
+        #     data_list.append(data)
+        # earth_sign, earth_degree = get_sign_and_degree((planet_positions['Sun']['degree'] + 180) % 360)
+        # planet_positions['Earth'] = {"sign": earth_sign, "degree": earth_degree}
+
+        position_dms = decdeg2dms((sun['position']+180)%360)
+        degree = position_dms[0]
+        minutes = position_dms[1]
+        seconds = round(position_dms[2])
+        data = {
+                'item' :item_t,
+                'Name': "earth",
+                'Sign': None,
+                'Pos_dec' : (sun['position']+180)%360,
+                'Degree': degree,
+                'Minutes': minutes,
+                'Seconds': seconds
+            }
+        data_list.append(data)
     # Create a DataFrame from the list of dictionaries
     df_kerykeion = pd.DataFrame(data_list)
 
     # Print the DataFrame
+    print("df_kerykeion")
     print(df_kerykeion)
     # print(sun_position_at_birth_dec)
     return df_kerykeion
@@ -165,6 +199,8 @@ def generate_report(df):
         #decimalDegrees = df.iloc[i]["Pos_dec"]
         decimalDegrees = df.iloc[i]["Degree"]  +  df.iloc[i]["Minutes"] / 60  +  df.iloc[i]["Seconds"] / 3600
         sign = df.iloc[i]["Sign"]
+
+        # https://github.com/jdempcy/hdkit/blob/main/sample-apps/v1/substructure.js
         signDegrees =0
         for iteratingSign in allSigns:
             if iteratingSign == sign:
@@ -172,11 +208,14 @@ def generate_report(df):
             signDegrees += 30  
 
         signDegrees += decimalDegrees
+
+        # // Human Design gates start at Gate 41 at 02º00'00" Aquarius, so we have to adjust from 00º00'00" Aries.
+		# // The distance is 58º00'00" exactly.
         signDegrees += 58
         if (signDegrees > 360):
             signDegrees -= 360
         percentageThrough = (signDegrees / 360)
-        gate = gateOrder[int(percentageThrough * 64)]
+        gate = gateOrder[int(percentageThrough * 64)] # in the app is 1 decimal
         
         exactLine = 384 * percentageThrough
         line = (exactLine % 6) + 1
@@ -210,8 +249,31 @@ def generate_report(df):
 
     # Create a DataFrame from the list of dictionaries
     df_result = pd.DataFrame(data_list)
+    print ("------")
+    print ("df_result")
     print (df_result)
-        
+    make_chart(df_result)
+
+def make_chart(df_result):
+    """Makes the chart. If the number is surrounde by bracket, that number is in a colored circle. If not, it's not
+
+    Args:
+        df_result (_type_): _description_
+    """
+
+    gates_chart= df_result["Gate"].to_list()
+   
+    gates= [[64,61,63], [47,24,4,17,11,43],[6223,56,35,12,45,33,8,31,20,16],
+            [1,13,25,46,2,15,10,7],[21,40,26,51],[48,57,44,50,32,29,18],
+            [59,9,3,42,27,34,5,14,29],[36,22,37,6,49,55,30],[41,39,19,52,60,53,54,38,58]]
+    for g in gates:
+        for h in g:
+            if h in gates_chart:
+                print (f"[{h}]", end = " ")
+            else:
+                print (h , end = " ")
+        print("")
+
 def main():
     y,m,d,h,min = 1983, 9, 25, 20, 48
     birthplace = "Malden, MA (US)"
@@ -219,13 +281,14 @@ def main():
     delta = 7
     df_kerykeion = main_kerykeion(y,m,d,h,min, birthplace, birthcountry, delta)
     generate_report(df_kerykeion)
-
+    # expected output 
+    # https://github.com/jdempcy/hdkit/blob/main/sample-apps/v1/sample-data/charts/jonah-dempcy-chart.js
 def mainx():
-    import ephem 
+    
     #y,m,d,h,min = 1983, 9, 25, 20, 48
     y,m,d,h,min = 1983, 9, 26, 3, 48
 
-    from skyfield.api import load
+    
 
     # Create a timescale and ask the current time.
     ts = load.timescale()
@@ -245,3 +308,4 @@ def mainx():
     
 if __name__ == "__main__":
     main()
+    mainx()
