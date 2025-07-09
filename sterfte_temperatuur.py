beeb5516697dbbcee8e173aeae3e0d824dd85e59
Rene Smit <rcx.smit@gmail.com>
2025-07-09 19:51:37 +0200

    ...

--- sterfte_temperatuur.py
+++ sterfte_temperatuur.py
@@ -22,8 +22,8 @@ def get_weather_info():
     current_datetime = datetime.now()
     formatted_date = current_datetime.strftime("%Y%m%d")
 
-    #url_nw_beerta = f"https://www.daggegevens.knmi.nl/klimatologie/daggegevens?stns=260&vars=TEMP:SQ:SP:Q:DR:RH:UN:UX&start=20000101&end={formatted_date}"
-    url_nw_beerta = "https://raw.githubusercontent.com/rcsmit/streamlit_scripts/main/input/nw_beerta.csv"
+    url_nw_beerta = f"https://www.daggegevens.knmi.nl/klimatologie/daggegevens?stns=260&vars=TEMP:SQ:SP:Q:DR:RH:UN:UX&start=20000101&end={formatted_date}"
+    #url_nw_beerta = "https://raw.githubusercontent.com/rcsmit/streamlit_scripts/main/input/nw_beerta.csv"
     df_nw_beerta = pd.read_csv(
         url_nw_beerta,
         delimiter=",",
@@ -97,7 +97,7 @@ def get_weather_info():
     st.write(f"Gmiddelde temperatuur week 3-10 : {round(mean_3_10,1)}")
     return result
 
-
+@st.cache_data()
 def get_sterfte():
     """_summary_
 
@@ -108,13 +108,18 @@ def get_sterfte():
     # https://ec.europa.eu/eurostat/databrowser/bookmark/fbd80cd8-7b96-4ad9-98be-1358dd80f191?lang=en
     # https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/dataflow/ESTAT/DEMO_R_MWK_05/1.0?references=descendants&detail=referencepartial&format=sdmx_2.1_generic&compressed=true
     file = "https://raw.githubusercontent.com/rcsmit/streamlit_scripts/main/input/sterfte_eurostats_weekly__2000_01__2023_41.csv"
-
+    #file = r"C:\Users\rcxsm\Documents\python_scripts\streamlit_scripts\input\sterfte_eurostats_weekly__2000_01__2025_14.csv"
+    file = "https://raw.githubusercontent.com/rcsmit/streamlit_scripts/main/input/sterfte_eurostats_weekly__2000_01__2025_14.csv"
+    
+    api_link = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/3.0/data/dataflow/ESTAT/demo_r_mwk_05/1.0/*.*.*.*.*?c[freq]=W&c[age]=TOTAL,Y_LT5,Y5-9,Y10-14,Y15-19,Y20-24,Y25-29,Y30-34,Y35-39,Y40-44,Y45-49,Y50-54,Y55-59,Y60-64,Y65-69,Y70-74,Y75-79,Y80-84,Y85-89,Y_GE90,UNK&c[sex]=T,M,F&c[unit]=NR&c[geo]=NL&c[TIME_PERIOD]=ge:2000-W01&compress=false&format=csvdata&formatVersion=1.0&lang=en&labels=both"
     df_ = pd.read_csv(
-        file,
+        api_link,
         delimiter=",",
         low_memory=False,
     )
-
+    for col in ['freq', 'age', 'sex', 'unit', 'geo']:
+        df_[[col, f'{col}_desc']] = df_[col].str.split(':', n=1, expand=True)
+ 
     df_["age_sex"] = df_["age"] + "_" + df_["sex"]
     df_ = df_[df_["age_sex"] == "TOTAL_T"]
     # st.write(df_["age_sex"].unique())
@@ -128,6 +133,8 @@ def get_sterfte():
     df_ = df_[["year_number", "year_week", "week_number", "OBS_VALUE"]]
     df_["obs_2461"] = round(df_["OBS_VALUE"] / 2461 * 100, 1)
     df_["obs_3042"] = round(df_["OBS_VALUE"] / 3042 * 100, 1)
+    # We hebben ook de gemiddelde temperatuur tussen week 3 en 10 berekend (te weten 10,85 graad) en de daarbijbehorende verwachte sterfte genomen vanuit grafiek 2 (3042) en deze op 100 gesteld
+    # We hebben de gemiddelde sterfte genomen van alle weken tussen week 3 tot en met en 10 (te weten 3055 personen) en dit op 100 gesteld
     df_["OBS_sma"] = df_["OBS_VALUE"].rolling(window=5).mean()
     df_week3_10 = df_[(df_["week_number"] >= 3) & (df_["week_number"] <= 10)]
 
@@ -144,16 +151,24 @@ def make_scatter(x, y, df, title):
         merged_df (str): df
     """
     st.subheader(title)
-    fig = px.scatter(
+    fig_ = px.scatter(
         df,
         x=x,
         y=y,
         hover_data=["year_number", "week_number"],
         color="year_number",
         title=title,
-        trendline="ols",
+        
     )  #  trendline_scope="overall", labels={'datum': 'Date', 'verbruik': 'Verbruik'})
-    st.plotly_chart(fig, use_container_width=True)
+    fig = px.scatter(
+        df,
+        x=x,
+        y=y,
+        color="year_number",
+        hover_data=["year_number", "week_number"],
+        title=title,
+    )  #  trendline_scope="overall", labels={'datum': 'Date', 'verbruik': 'Verbruik'})
+    st.plotly_chart(fig_, use_container_width=True)
     # Calculate the correlation
     try:
         correlation = df[x].corr(df[y])
@@ -245,7 +260,7 @@ def decomposed(merged_df):
     correlation_coefficient, p_value = pearsonr(
         merged_df["temp_avg_adj"], merged_df["OBS_VALUE"]
     )
-
+   
     make_scatter("temp_avg_adj", "OBS_VALUE", merged_df, "all")
     st.write(f"Correlation Coefficient: {correlation_coefficient:.2f}")
     st.write(f"P-value: {p_value:.4f}")
@@ -308,10 +323,11 @@ def main():
     df_sterfte = get_sterfte()
     df_temperature = get_weather_info()
     df = df_sterfte.merge(df_temperature, on=["year_number", "week_number"])
-    df = df[df["year_number"] < 2020]
+    #df = df[df["year_number"] < 2020]
 
     # calculate the average for each week for all 20 years and merge it
-    df_avg_week = df_sterfte.groupby("week_number").mean().reset_index()
+    df_avg_week = df_sterfte.groupby("week_number", as_index=False).mean(numeric_only=True)
+
     df_avg_week.rename(columns={"OBS_VALUE": "OBS_MEAN"}, inplace=True)
     df_avg_week = df_avg_week[["week_number", "OBS_MEAN"]]
     df = df.merge(df_avg_week, on="week_number", how="outer")
@@ -332,13 +348,24 @@ def main():
     # Create a new column 'DISTANCE_FROM_AVG' with the distance between OBS_VALUE and
     # the average for each year
     df["DISTANCE_FROM_AVG"] = df["OBS_VALUE"] - df["AVG_OBS_VALUE_YEAR"]
+    col1, col2 = st.columns(2)
+    with col1:
+        make_scatter("temp_avg", "temp_max", df, "temp_max vs temp_avg")
+    with col2:
+        make_scatter("temp_avg", "temp_min", df, "temp_min vs temp_avg")
+  
+    what = st.sidebar.selectbox("what", ["temp_min", "temp_avg", "temp_max"],1)
 
-    st.write(df)
-    afkap = 16.5
-    what = "temp_avg"
+    if what=="temp_avg":
+        afkap = 16.5
+    elif what=="temp_min":
+        afkap =  0.84 * 16.5 + -2.60
+    
+    else:
+        afkap = 21.3  #  [ 1.14 * temp_avg + 2.49]
     df_lower = df[df[what] <= afkap].copy(deep=True)
     df_higher = df[df[what] > afkap]
-
+    st.write(df)
     make_scatter(what, "OBS_VALUE", df, "all")
     col1, col2 = st.columns(2)
     with col1:
