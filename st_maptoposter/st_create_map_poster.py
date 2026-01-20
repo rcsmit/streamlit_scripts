1af7b7d044bbf1c53671d93419edad6fdcb64612
Rene Smit <rcx.smit@gmail.com>
2026-01-20 21:47:25 +0700

    ...

--- st_maptoposter/st_create_map_poster.py
+++ st_maptoposter/st_create_map_poster.py
@@ -137,7 +137,7 @@ def get_available_themes_streamlit_sharing():
     return themes
 
 #@st.cache_data
-def load_theme__streamlit_sharing(theme_name="feature_based"):
+def load_theme_streamlit_sharing(theme_name="feature_based"):
     """Load theme from JSON file in themes directory."""
     theme_file = THEMES_DIR / f"{theme_name}.json"
     
