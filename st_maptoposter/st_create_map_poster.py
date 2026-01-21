00c0aa0841e00e472178c6da6f1b6ee8b6ba2845
Rene Smit <rcx.smit@gmail.com>
2026-01-21 13:37:17 +0700

    ,,

--- st_maptoposter/st_create_map_poster.py
+++ st_maptoposter/st_create_map_poster.py
@@ -26,6 +26,8 @@ POSTERS_DIR.mkdir(exist_ok=True)
 
 # Pre-defined city coordinates (no API needed!)
 CITY_COORDINATES = {
+    "Da Nang, Vietnam": (16.06778, 108.22083),
+    "Hoi An, Vietnam": (15.88006, 108.33804),
     "Stadskanaal, Netherlands":(52.996700, 6.895670),
     "Amsterdam, Netherlands": (52.3676, 4.9041),
     "New York, USA": (40.7128, -74.0060),
