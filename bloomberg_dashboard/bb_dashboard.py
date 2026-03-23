0c712504f976d0688c68172845d11c3e8b7f6e87
Rene Smit <rcx.smit@gmail.com>
2026-03-23 13:47:06 +0700

    ...

--- bloomberg_dashboard/bb_dashboard.py
+++ bloomberg_dashboard/bb_dashboard.py
@@ -6,23 +6,29 @@ from uitleg_app import *
 from uitleg_backtest import *
 from bloomberg_template import *
 
-tab1,tab2,tab3, tab4,tab5,tab6=st.tabs(["dashboard", "backtest","uitleg app", "uitleg backtest", "info","template"])
+def main():
+    tab1,tab2,tab3, tab4,tab5,tab6=st.tabs(["dashboard", "backtest","uitleg app", "uitleg backtest", "info","template"])
 
-with tab3:
-    uitleg_app()
-    
-with tab4:
-    uitleg_backtest()
+    with tab3:
+        uitleg_app()
+        
+    with tab4:
+        uitleg_backtest()
 
 
-with tab6:
-    bloomberg_template()
+    with tab6:
+        bloomberg_template()
 
-with tab1:
-    dashboard()
-    
-with tab2:
-    backtest()
+    with tab1:
+        dashboard()
+        
+    with tab2:
+        backtest()
 
-with tab5:
-    st.info("Vibecoded with ClaudeAI, prompt by @investingluc: https://x.com/investingluc/status/2034403688106615050")
+    with tab5:
+        st.info("Vibecoded with ClaudeAI, prompt by @investingluc: https://x.com/investingluc/status/2034403688106615050")
+
+
+        
+if __name__ == "__main__":
+    main()
