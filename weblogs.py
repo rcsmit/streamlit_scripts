5f49a904725d6a3f141859f8e50352b660e30871
Rene Smit <rcx.smit@gmail.com>
2026-03-24 14:34:16 +0700

    .....

--- weblogs.py
+++ weblogs.py
@@ -1,57 +1,23 @@
 import pandas as pd
 import streamlit as st
 
+@st.cache_data()
 def read():
-    #filetype = 'google_sheet'
-    filetype = 'xls'
-    #file = 'C:\Users\rcxsm\Documents\pyhton_scripts'
+    filetype = 'google_sheet'
 
-    if filetype == 'csv':
-        try:
-            df = pd.read_csv(
-                "masterfinance.csv",
-                names=["id","bron","datum","bedrag",
-                       "tegenpartij","hoofdrub","rubriek"],
-
-                dtype={
-                    "bron": "category",
-                    "hoofdrub": "category",
-                    "rubriek": "category",
-
-                },
-                delimiter=';',
-                parse_dates=["datum"],
-                encoding='latin-1'  ,
-                dayfirst=True
-            )
-        except:
-            st.warning("error met laden")
-            st.stop()
-    elif filetype == 'xls':
-        file = "https://github.com/rcsmit/streamlit_scripts/raw/main/input/entry_all_blogs.xlsx"
-        #file = r"input\entry_all_blogs.xlsx"
-        sheet = "entry"
-        try:
-            df = pd.read_excel (file,
-                                sheet_name= sheet,
-                                header=0,
-                                usecols= "a,b,c,d,e,f,g,h,i",
-                                names=["id","id_entry_original","titel","kopfoto","artikel","datum","afbeelding","link","blog"])
-            #df["datum"] = pd.to_datetime(df["datum"], format="%Y-%m-%d")
-            df.datum=pd.to_datetime(df.datum,errors='coerce',format="%d/%m/%Y  hh:mm:ss", dayfirst=True)
-            
-        except Exception as e:
-            st.warning("error met laden")
-            st.warning(f"{e}")
-            st.warning(traceback.format_exc())
-            st.stop()
-
-    elif filetype =='google_sheet':
+    if filetype =='google_sheet':
         sheet_name = "gegevens"
+        sheet_id = "1R5YDxVqpT1brUHz1P-Zjoyz0iHgLBJUIsSrbH9IfW5c"
+        # https://docs.google.com/spreadsheets/d/1R5YDxVqpT1brUHz1P-Zjoyz0iHgLBJUIsSrbH9IfW5c/edit?usp=sharing
         url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
-        df = pd.read_csv(url, delimiter=',')
-        st.write(df.dtypes)
-        df["datum"] = pd.to_datetime(df["datum"], format="%d-%m-%Y")
+        df = pd.read_csv(url, delimiter=',', header=0,
+                                 usecols=list(range(10)),
+                                names=["id","id_entry_original","titel","kopfoto","artikel","datum","afbeelding","link","blog","categorie"])
+      
+        try:
+            df["datum"] = pd.to_datetime(df["datum"], format="%d-%m-%Y")
+        except:
+            df["datum"] = pd.to_datetime(df["datum"], format='mixed')
         # df = df[:-1]  #remove last row which appears to be a Nan
     else:
         st.write("type doesnt exist")
@@ -73,23 +39,31 @@ def main():
     df['artikel'] = df['artikel'].replace('<P>','')
     df['artikel'] = df['artikel'].replace('</P>','/n')
     # Multiselect widget
-    options = ["weblog", "fotolog", "petitmonde", "horecalog"]
+    options = df["blog"].unique().tolist()
     selected_blogs = st.sidebar.multiselect("Select blog types", options, options)
 
     # Filter DataFrame based on selection
     if selected_blogs:
         df = df[df["blog"].isin(selected_blogs)]
+
     else:
         st.error("Select one of more blogs")
         st.stop()
+    if (len(selected_blogs)==1 and ((selected_blogs == ["CrazyWaiter"]) or (selected_blogs==["YepYoga"]))):
+        categories= df["categorie"].unique().tolist()
+        selected_categories = st.sidebar.multiselect("Select categories", categories, categories)
 
-
+        df = df[df["categorie"].isin(selected_categories)]
+        if len(df)==0:
+            st.error("Choose a category")
     for index, row in df.iterrows():
         # st.write(f"Row {index}:")
         # st.write(f"id: {row['id']}")
         # st.write(f"id_entry_original: {row['id_entry_original']}")
         st.subheader(f"{row['titel']}")
         st.write(f"{row['datum']}")
+        if row['categorie'] != "_":
+            st.write(f"Categorie: {row['categorie']}")  
         if row['kopfoto'] != "_":
             #image_url = f"https://github.com/rcsmit/streamlit_scripts/tree/main/printbak/thumbnails/{row['kopfoto']}?raw=true"
             image_url = f"https://raw.githubusercontent.com/rcsmit/streamlit_scripts/main/printbak/thumbnails/{row['kopfoto']}"
