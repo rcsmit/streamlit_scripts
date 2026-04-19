7af7f9fd3377ced03aceaf05753ea6ccf52cb59a
Rene Smit <rcx.smit@gmail.com>
2026-04-20 01:55:16 +0700

    ...

--- loess_viewer.py
+++ loess_viewer.py
@@ -210,7 +210,7 @@ def main():
             span = min(max(span, 0.001), 1.0)
 
             y_loess = loess_smooth(x_use, y_use, span)
-            y_sma= pd.Series(y_use).rolling(window=window, min_periods=1, center=True).mean().to_numpy()
+            y_sma= pd.Series(y_use).rolling(window=window, center=True).mean().to_numpy()
             print (y_loess)
             if first:
                 fig.add_trace(
